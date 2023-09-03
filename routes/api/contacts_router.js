a4a0f22b06591f2a6175e850c640b328e9a79de6
Iryna Artymuk <artimyk@ukr.net>
2023-09-03 21:59:52 +0300

    feat(contacts router): add new method
    add new method get to router  method router.get makes request to db and return all contacts list

--- routes/api/contacts_router.js
+++ routes/api/contacts_router.js
@@ -5,23 +5,31 @@ import contactsService from "../../models/contacts.js"
 const router = express.Router()// create router 
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  try {
+    const result = await contactsService.getContactsList()
+    res.json(result);
+    
+  } catch (error) {
+    next(error);
+  }
+
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.get('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.post('/', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.delete('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.put('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
 export default  router
