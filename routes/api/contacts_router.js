3f6cbf87eef19bb797944cfe62c3f1511b4fca61
Iryna Artymuk <artimyk@ukr.net>
2023-09-04 15:39:43 +0300

    feat(controlers): add new folder contrllers
    add folder controllers moved add func from contact router to contrillers from controllers export
    object use it in contacts router

--- routes/api/contacts_router.js
+++ routes/api/contacts_router.js
@@ -1,119 +1,16 @@
 import express from 'express';
-import Joi from 'joi'; // бібліотека валідації
 
-import { contactsService } from '../../models/index.js';
-import { HttpError } from '../../helpers/index.js';
-import { normalisePhoneNumber } from '../../helpers/index.js';
-
-const contactAddSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-
-  email: Joi.string()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ['com', 'net', 'org'] },
-    })
-    .message('only allowed domain name .com .net .org ')
-    .required(),
-
-  phone: Joi.string()
-    .regex(
-      /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
-    )
-    .message(
-      ' mobile number must have 10 digit  be valid, exemple (000)-000-0000 or (000)0000000 or 0000000000 '
-    )
-    .required(),
-});
+import contactsControler from '../../controllers/contact_controler.js';
 const router = express.Router(); // create router
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await contactsService.getContactsList();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/', contactsControler.getAllContacts);
 
-router.get('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  try {
-    const result = await contactsService.getContactById(contactId);
+router.get('/:contactId', contactsControler.getContactById);
 
-    // якщо id буде не правильний то moviesService.getMovieById поверне null (так працює база даних )null це не помилка а нам треба щоб коли id не вірний тобто коли база нічого не знайшла треба створти повертати помилку
-    if (!result) {
-      throw HttpError(
-        404,
-        `контакту з id:${id} не знайдено перевірте чи правильний id `
-      );
-    }
+router.post('/', contactsControler.addContact);
 
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put('/:contactId', contactsControler.updateContactById);
 
-router.post('/', async (req, res, next) => {
-  try {
-    const validateResult = contactAddSchema.validate(req.body);
-    const { error } = validateResult;
-
-    if (error) throw HttpError(422, error.message);
-    const result = await contactsService.addContact({
-      ...req.body,
-      phone: normalisePhoneNumber(req.body.phone),
-    });
-
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  try {
-    const validateResult = contactAddSchema.validate(req.body);
-    const { error } = validateResult;
-
-    if (error) throw HttpError(422, error.message);
-
-    const updateContact = {
-      ...req.body,
-      phone: normalisePhoneNumber(req.body.phone),
-    };
-
-    const result = await contactsService.updateContactById(
-      contactId,
-      updateContact
-    );
-
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  try {
-    const result = await contactsService.deleteContactById(contactId);
-
-    if (!result) {
-      throw HttpError(
-        404,
-        `контакту з id:${contactId} не знайдено перевірте чи правильний id `
-      );
-    }
-
-    res.json({
-      message: ` Contact with id:${contactId} deleted successfully`,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete('/:contactId', contactsControler.deleteContactById);
 
 export default router;
