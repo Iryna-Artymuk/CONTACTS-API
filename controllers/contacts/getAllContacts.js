5da67424942dd23da571c27914fa72c21c22f6d3
Iryna Artymuk <artimyk@ukr.net>
2023-09-19 08:30:33 +0300

    feat(controllers): add pagination and favrit filter

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,18 +1,15 @@
 import asyncHandler from '../../decorators/acyncHandler.js';
 import Contact from '../../models/contacts.js';
 
-const getAllContacts = async (req, res, next) => {
-  const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query; // деструктуризуємо параметри пошуку з фронтенду
+const getAllContacts = async (req, res) => {
+  const { page = 1, limit = 10, favorite = true } = req.query; // деструктуризуємо параметри пошуку з фронтенду
   // додаємо ці параметри до запиту до БД
   const skip = (page - 1) * limit; // скільки обєктів пропустити з початку бази
-  const result = await Contact.find(
-    { owner },
-    {
-      skip,
-      limit,
-    }
-  ).populate('owner');
+
+  const result = await Contact.find({ favorite }, null, {
+    skip,
+    limit,
+  }).populate('owner');
   res.json({ data: result, quantity: result.length });
 };
 
