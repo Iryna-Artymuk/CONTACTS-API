063fabe89529fba5a21d2e4ccec19bff778fdf62
Iryna Artymuk <artimyk@ukr.net>
2023-09-19 07:15:15 +0300

    feat(gett all contacts): add pagination to contacts

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -2,7 +2,17 @@ import asyncHandler from '../../decorators/acyncHandler.js';
 import Contact from '../../models/contacts.js';
 
 const getAllContacts = async (req, res, next) => {
-  const result = await Contact.find({});
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query; // деструктуризуємо параметри пошуку з фронтенду
+  // додаємо ці параметри до запиту до БД
+  const skip = (page - 1) * limit; // скільки обєктів пропустити з початку бази
+  const result = await Contact.find(
+    { owner },
+    {
+      skip,
+      limit,
+    }
+  ).populate('owner');
   res.json({ data: result, quantity: result.length });
 };
 
