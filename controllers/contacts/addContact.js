ef898b6de7f950ddf8ed69ccdacd57b93237050e
Iryna Artymuk <artimyk@ukr.net>
2023-09-21 11:04:23 +0300

    feat(add contact controller): add gravatar
    add avatart fild to contact model creat gravatar url

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,13 +1,19 @@
+import gravatar from 'gravatar';
+
 import asyncHandler from '../../decorators/acyncHandler.js';
 import { normalisePhoneNumber } from '../../helpers/index.js';
 import Contact from '../../models/contacts.js';
 
 const addContact = async (req, res, next) => {
   const { user } = req;
+  const { email } = req.body;
   const { _id: ownerId } = user;
+  const gravatarURL = gravatar.url(email);
+  console.log('gravatarURL : ', gravatarURL);
+
   const result = await Contact.create({
     ...req.body,
-    owner: ownerId ,
+    owner: ownerId,
     phone: normalisePhoneNumber(req.body.phone),
   });
 
